16b05b0cd9dea3d58365962cdfab9c393f02726b
Elijah Brown <elijahab716@gmail.com>
2024-06-25 10:31:44 -0500

    added basic markdown template

---
+++ 02-Challenge/Develop/generate.md
@@ -0,0 +1,22 @@
+# ${response.title}
+
+## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Test Instructions](#testing-instructions)
+    - [Questions](#questions)
+## Description
+    ${response.description}
+
+### Installation
+
+### Usage
+
+### Contributing
+
+### Testing Instructions
+
+### Questions
+
--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -4,7 +4,47 @@ const inquirer = require('inquirer')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What would you like to name the file?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please enter installation instructions: ',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage information: ',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Please enter contribution guidelines: ',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Please enter test instructions: ',
+        name: 'test'
+    }
+])
+    .then((response) => {
+        console.log(response.name)
+        console.log(response.description)
+        console.log(response.installation)
+        console.log(response.usage)
+        console.log(response.contribution)
+        console.log(response.test)
+        return response
+    })
  
 
 // TODO: Create a function to write README file
