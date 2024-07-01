bb9612b76040ffea9b39ef3d8ae884bcb86b3671
Elijah Brown <elijahab716@gmail.com>
2024-07-01 08:19:19 -0500

    fixed writeToFile function

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -58,7 +58,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('generate.md', readmeContent, err => 
+    fs.writeFile(fileName, data, err => 
         err ? console.error('Document creation failed.') : console.log('Document created successfully!')
     );
 }
